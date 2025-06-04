e9e0e547a3e2e4d7e854c5c830fb5d133d6a8140
nrk16p <38444994+nrk16p@users.noreply.github.com>
2025-06-04 08:09:17 +0700

    Update car_scraper_py.py

--- car_scraper_py.py
+++ car_scraper_py.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import shutil
 
 options = Options()
@@ -8,16 +9,19 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument("--window-size=1920,1080")
-options.binary_location = "/usr/bin/google-chrome"
 
-
+# Detect chrome binary
 chrome_path = shutil.which("google-chrome") or shutil.which("google-chrome-stable")
 print("✅ Chrome found:", chrome_path)
+
 if chrome_path:
     options.binary_location = chrome_path
-    driver = webdriver.Chrome(service=Service(), options=options)
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
     driver.get("https://google.com")
-    print(driver.title)
+    print("✅ Page title:", driver.title)
     driver.quit()
 else:
     print("❌ Chrome not found")
